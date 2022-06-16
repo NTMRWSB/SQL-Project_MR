93280fa3f843a7f03554c606cc4696ab75f0b99d
NTMRWSB <wzx20020111@outlook.com>
2022-06-16 10:52:32 +0800

    update docker account

--- run_with_root.sql
+++ run_with_root.sql
@@ -1,7 +1,16 @@
 CREATE USER 'admin' @'localhost' IDENTIFIED BY '123456';
 
+--docker
+--CREATE USER 'admin' @'172.17.0.1' IDENTIFIED BY '123456';
+
 ALTER USER 'admin' @'localhost' IDENTIFIED WITH mysql_native_password BY '123456';
+
+--docker
+--ALTER USER 'admin' @'172.17.0.1' IDENTIFIED WITH mysql_native_password BY '123456';
 
 CREATE DATABASE IF NOT EXISTS supermr_db;
 
 GRANT ALL PRIVILEGES ON supermr_db.* TO 'admin' @'localhost';
+
+--docker
+--GRANT ALL PRIVILEGES ON supermr_db.* TO 'admin' @'172.17.0.1';
